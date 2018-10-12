6573170b155da49f72745d186aade848c2b022a3
ferAlvarado <fernandaalvaradov@hotmail.com>
2018-10-11 21:29:38 -0600

    Interfaz con sonido
    No encontré todos los sonidos de las teclas

---
+++ p2/UI/Proyecto-II-IA/GUI_IA/src/gui_ia/GUI_IA.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui_ia;
+
+import java.io.IOException;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+/**
+ *
+ * @author Fer Alvarado
+ */
+public class GUI_IA {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Inicio ini= new Inicio();
+        ini.setVisible(true);
+    }
+    
+}
